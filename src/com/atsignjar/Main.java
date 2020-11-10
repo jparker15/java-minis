6da920be6097b72eac6781f29e2e891a9e419a7c
Jeremy <jeremyeparker@yahoo.com>
2020-11-09 23:22:09 -0500

    first commit

---
+++ src/com/atsignjar/Main.java
@@ -0,0 +1,14 @@
+package com.atsignjar;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
